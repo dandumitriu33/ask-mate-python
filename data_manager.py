c8d03c7e8a5355e62c6e1523766549ff736a5154
Andreea Grosu <andreea.grosu87@gmail.com>
2019-12-10 09:59:48 +0200

    Adapted sorting function for any order keys

--- data_manager.py
+++ data_manager.py
@@ -1,8 +1,8 @@
 import connection
 
-def sort_questions():
+def sort_questions(order_key='submission_time', reversed=True):
     questions = connection.get_all_questions(convert_linebreaks=True)
-    sorted_questions = sorted(questions, key = lambda i: i['submission_time'], reverse=True)
+    sorted_questions = sorted(questions, key = lambda i: i[order_key], reverse=reversed)
     return sorted_questions
 
 
--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/list')
 def route_list():
-    questions = data_manager.sort_questions()
+    questions = data_manager.sort_questions('submission_time', True)
     return render_template('list.html', questions=questions)
 
 
