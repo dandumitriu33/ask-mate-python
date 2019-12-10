d298ce56f5b589f9662553770e20e2267bc11964
Andreea Grosu <andreea.grosu87@gmail.com>
2019-12-10 13:57:57 +0200

    Fix list all questions feature - sorted

--- data_manager.py
+++ data_manager.py
@@ -3,7 +3,7 @@ import time
 
 
 def sort_questions(order_key='submission_time', reversed=True):
-    questions = connection.get_all_questions(convert_linebreaks=True)
+    questions = connection.get_data()
     sorted_questions = sorted(questions, key = lambda i: i[order_key], reverse=reversed)
     return sorted_questions
 
