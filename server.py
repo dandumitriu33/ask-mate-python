c9d0ae46d2f8d5ee41594c2b71ad1aca436843be
iulian-coder <iulian.titei@gmail.com>
2019-12-11 14:16:12 +0200

    upload in progress

--- server.py
+++ server.py
@@ -1,71 +1,85 @@
-from flask import Flask, render_template, request, redirect
+import os
+from flask import Flask, render_template, request, redirect, send_from_directory
+from werkzeug.utils import secure_filename
 import data_manager
 
+UPLOAD_FOLDER = '/home/iulian/PycharmProjects/ask-mate-python/static/img'
+ALLOWED_EXTENSIONS = {'png', 'jpg'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 LAST_VISITED_QUESTION = 0
 
-@app.route('/')
-@app.route('/list')
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/list', methods=['GET', 'POST'])
 def route_list():
-    sort_by = request.args.get(key='order_by')
-    order_direction = request.args.get(key='order_direction')
-    if sort_by == 'submission time' and order_direction == 'desc':
-        questions = data_manager.sort_questions('submission_time', True)
-        return render_template('list.html',
-                               category=sort_by,
-                               questions=questions)
-    elif sort_by == 'submission time' and order_direction == 'asc':
-        questions = data_manager.sort_questions('submission_time', False)
-        return render_template('list.html',
-                               category=sort_by,
-                               questions=questions)
-    elif sort_by == 'title' and order_direction == 'desc':
-        questions = data_manager.sort_questions('title', True)
-        return render_template('list.html',
-                               category=sort_by,
-                               questions=questions)
-    elif sort_by == 'title' and order_direction == 'asc':
-        questions = data_manager.sort_questions('title', False)
-        return render_template('list.html',
-                               category=sort_by,
-                               questions=questions)
-    elif sort_by == 'message' and order_direction == 'desc':
-        questions = data_manager.sort_questions('message', True)
-        return render_template('list.html',
-                               category=sort_by,
-                               questions=questions)
-    elif sort_by == 'message' and order_direction == 'asc':
-        questions = data_manager.sort_questions('message', False)
-        return render_template('list.html',
-                               category=sort_by,
-                               questions=questions)
-    elif sort_by == 'view number' and order_direction == 'desc':
-        questions = data_manager.sort_questions('view_number', True)
-        return render_template('list.html',
-                               category=sort_by,
-                               questions=questions)
-    elif sort_by == 'view number' and order_direction == 'asc':
-        questions = data_manager.sort_questions('view_number', False)
-        return render_template('list.html',
-                               category=sort_by,
-                               questions=questions)
-    elif sort_by == 'vote number' and order_direction == 'desc':
-        questions = data_manager.sort_questions('vote_number', True)
-        return render_template('list.html',
-                               category=sort_by,
-                               questions=questions)
-    elif sort_by == 'vote number' and order_direction == 'asc':
-        questions = data_manager.sort_questions('vote_number', False)
-        return render_template('list.html',
-                               category=sort_by,
-                               questions=questions)
-    else:
-        questions = data_manager.get_data()
-        return render_template('list.html',
-                               category=sort_by,
-                               questions=questions)
+    if request.method == 'GET':
+        sort_by = request.args.get(key='order_by')
+        order_direction = request.args.get(key='order_direction')
+        if sort_by == 'submission time' and order_direction == 'desc':
+            questions = data_manager.sort_questions('submission_time', True)
+            return render_template('list.html',
+                                   category=sort_by,
+                                   questions=questions)
+        elif sort_by == 'submission time' and order_direction == 'asc':
+            questions = data_manager.sort_questions('submission_time', False)
+            return render_template('list.html',
+                                   category=sort_by,
+                                   questions=questions)
+        elif sort_by == 'title' and order_direction == 'desc':
+            questions = data_manager.sort_questions('title', True)
+            return render_template('list.html',
+                                   category=sort_by,
+                                   questions=questions)
+        elif sort_by == 'title' and order_direction == 'asc':
+            questions = data_manager.sort_questions('title', False)
+            return render_template('list.html',
+                                   category=sort_by,
+                                   questions=questions)
+        elif sort_by == 'message' and order_direction == 'desc':
+            questions = data_manager.sort_questions('message', True)
+            return render_template('list.html',
+                                   category=sort_by,
+                                   questions=questions)
+        elif sort_by == 'message' and order_direction == 'asc':
+            questions = data_manager.sort_questions('message', False)
+            return render_template('list.html',
+                                   category=sort_by,
+                                   questions=questions)
+        elif sort_by == 'view number' and order_direction == 'desc':
+            questions = data_manager.sort_questions('view_number', True)
+            return render_template('list.html',
+                                   category=sort_by,
+                                   questions=questions)
+        elif sort_by == 'view number' and order_direction == 'asc':
+            questions = data_manager.sort_questions('view_number', False)
+            return render_template('list.html',
+                                   category=sort_by,
+                                   questions=questions)
+        elif sort_by == 'vote number' and order_direction == 'desc':
+            questions = data_manager.sort_questions('vote_number', True)
+            return render_template('list.html',
+                                   category=sort_by,
+                                   questions=questions)
+        elif sort_by == 'vote number' and order_direction == 'asc':
+            questions = data_manager.sort_questions('vote_number', False)
+            return render_template('list.html',
+                                   category=sort_by,
+                                   questions=questions)
+        else:
+            questions = data_manager.get_data()
+            return render_template('list.html',
+                                   category=sort_by,
+                                   questions=questions)
+    elif request.method == 'POST':
+        file = request.files['file']
+        print(file)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return f"{filename}"
 
 
 @app.route('/question/<question_id>/new-answer', methods=['GET'])
@@ -123,6 +137,16 @@ def add_question():
     else:
         return render_template('/add-question.html')
 
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
